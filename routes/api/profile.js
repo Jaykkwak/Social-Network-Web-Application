0cff3adf70981de961af2eea8ca7d8c8c7e5f5a9
Jaykkwak <63373759+Jaykkwak@users.noreply.github.com>
2024-01-03 21:52:39 -0600

    Created Profile module and implemented profile/me route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
 
-//@route    GET api/profile
+//@route    GET api/profile/me
 //@desc     Test route
-//@access   Public
-router.get('/', (req, res) => res.send('Profile route'))
+//@access   Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    //get Profile
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
 
-module.exports = router
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+module.exports = router;
